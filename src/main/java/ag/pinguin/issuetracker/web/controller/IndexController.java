ccf4b74974d7055d5b855ffa47ebddccacbf2ce3
Mehdi Chitforoosh <mehdichitforoosh@gmail.com>
2021-12-26 19:52:05 +0330

    Add static ui css,js files and index controller to route to main page

---
+++ src/main/java/ag/pinguin/issuetracker/web/controller/IndexController.java
@@ -0,0 +1,24 @@
+package ag.pinguin.issuetracker.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Index Controller
+ *
+ * @author Mehdi Chitforoosh
+ */
+@Controller
+public class IndexController {
+
+    /**
+     * Index endpoint to show the index page
+     *
+     * @return view name
+     */
+    @GetMapping({"/", "/developers/**", "/issues/**", "/planner"})
+    public String index() {
+        return "index";
+    }
+
+}

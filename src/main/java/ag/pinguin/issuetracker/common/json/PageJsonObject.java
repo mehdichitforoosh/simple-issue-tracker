9a77b2827e0f331813821ccd39996326c86d830e
Mehdi Chitforoosh <mehdichitforoosh@gmail.com>
2021-12-26 19:49:20 +0330

    Add page json object to show list items

---
+++ src/main/java/ag/pinguin/issuetracker/common/json/PageJsonObject.java
@@ -0,0 +1,33 @@
+package ag.pinguin.issuetracker.common.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+/**
+ * Page Json Object
+ *
+ * @param <T>
+ * @author Mehdi Chitforoosh
+ */
+public class PageJsonObject<T> {
+
+    @JsonProperty(value = "totalItems", required = true)
+    private Long totalItems;
+
+    @JsonProperty(value = "items", required = true)
+    private List<T> items;
+
+    public PageJsonObject(Long totalItems, List<T> items) {
+        this.totalItems = totalItems;
+        this.items = items;
+    }
+
+    public Long getTotalItems() {
+        return totalItems;
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+}

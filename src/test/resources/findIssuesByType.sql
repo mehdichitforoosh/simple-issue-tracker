dae38fa500db5a152fc6782af2647be64364578e
Mehdi Chitforoosh <mehdichitforoosh@gmail.com>
2021-12-28 12:01:40 +0330

    Add test sql file for issue repository

---
+++ src/test/resources/findIssuesByType.sql
@@ -0,0 +1,10 @@
+INSERT INTO issues (issue_id, title, description, creation_date, issue_type, story_status, estimated_point)
+    VALUES (1L,'Add a button','','2021-12-26 22:37:46.23387', 'STORY', 'ESTIMATED', 9);
+INSERT INTO issues (issue_id, title, description, creation_date, issue_type, story_status, estimated_point)
+    VALUES (2L,'Add a text','','2021-12-26 22:38:46.23387', 'STORY', 'NEW', 3);
+INSERT INTO issues (issue_id, title, description, creation_date, issue_type, story_status, estimated_point)
+    VALUES (3L,'Remove the button','','2021-12-26 22:39:46.23387', 'BUG', 'NEW', 8);
+INSERT INTO issues (issue_id, title, description, creation_date, issue_type, story_status, estimated_point)
+    VALUES (4L,'Modify the button','','2021-12-26 22:40:46.23387', 'BUG', 'NEW', 8);
+INSERT INTO issues (issue_id, title, description, creation_date, issue_type, story_status, estimated_point)
+    VALUES (5L,'Edit the button','','2021-12-26 22:41:46.23387', 'BUG', 'NEW', 8);
